5214b83ca32d29dab13fa42fa9e494f13861b873
Tom Mortimer <tom@flax.co.uk>
2015-07-22 14:16:47 +0100

    fixed

--- compare_percolator/es-percolate-docs.py
+++ compare_percolator/es-percolate-docs.py
@@ -14,13 +14,14 @@ if __name__ == '__main__':
     for fname in os.listdir(sys.argv[1]):
         if fname.endswith('.gz'):
             with gzip.open(os.path.join(sys.argv[1], fname)) as f:
-                response = requests.post(conf.ES_URL + '/test/_percolate', 
+                response = requests.post(conf.ES_URL + '%s/doc/_percolate' % conf.ES_DB,
                     data=json.dumps({'doc': {'text': f.read()}}))
                 assert response.status_code == 200
                 total += response.json()['total']
                 count += 1
 
                 print fname, response.json()['total']
+
 
 #                 query_ids = [x['_id'] for x in response.json()['matches']]
 #                 query_ids.sort()
